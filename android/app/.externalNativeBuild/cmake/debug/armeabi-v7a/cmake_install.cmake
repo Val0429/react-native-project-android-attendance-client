6a75bd122c5f5638b54df10bf3e0a889ca39e217
Val Liu <val.liu@isapsolution.com>
2018-11-26 14:46:15 +0800

    stable

--- android/app/.externalNativeBuild/cmake/debug/armeabi-v7a/cmake_install.cmake
+++ android/app/.externalNativeBuild/cmake/debug/armeabi-v7a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /react-native/AttendanceClient/android/app
+# Install script for directory: /Users/valliu/Downloads/face_comp/android/app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
